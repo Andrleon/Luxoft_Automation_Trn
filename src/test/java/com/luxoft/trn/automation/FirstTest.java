c7bb59d5b86aad474324b2052d6f2268d9465604
andrleon <andreyko.l.a@gmail.com>
2018-09-11 21:37:49 +0300

    Initial commit

---
+++ src/test/java/com/luxoft/trn/automation/FirstTest.java
@@ -0,0 +1,17 @@
+package com.luxoft.trn.automation;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class FirstTest {
+
+    @Test
+    public void testSmth(){
+        boolean a = false;
+        assertFalse(a);
+
+
+    }
+
+
+}
